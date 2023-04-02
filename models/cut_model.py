0fef2860400fea3adf53b338c7675ef6b774aeaf
zchen <chenzze1@student.ubc.ca>
2023-04-02 10:35:25 -0700

    Update default hyper to current optimal

--- models/cut_model.py
+++ models/cut_model.py
@@ -24,8 +24,8 @@ class CUTModel(BaseModel):
 
         parser.add_argument('--lambda_GAN', type=float, default=1.0, help='weight for GAN loss：GAN(G(X))')
         parser.add_argument('--lambda_NCE', type=float, default=1.0, help='weight for NCE loss: NCE(G(X), X)')
-        parser.add_argument('--lambda_L1', type=float, default=0.0, help='weight for L1 loss: L1(G(X), gt)')
-        parser.add_argument('--lambda_SSIM', type=float, default=0.0, help='weight for SSIM loss: SSIM(G(X), gt)')
+        parser.add_argument('--lambda_L1', type=float, default=0.1, help='weight for L1 loss: L1(G(X), gt)')
+        parser.add_argument('--lambda_SSIM', type=float, default=1.0, help='weight for SSIM loss: SSIM(G(X), gt)')
         parser.add_argument('--nce_idt', type=util.str2bool, nargs='?', const=True, default=False, help='use NCE loss for identity mapping: NCE(G(Y), Y))')
         parser.add_argument('--nce_layers', type=str, default='0,4,8,12,16', help='compute NCE loss on which layers')
         parser.add_argument('--nce_includes_all_negatives_from_minibatch',
